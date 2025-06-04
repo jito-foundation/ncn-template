a2cbf307716ceb133015b3674b7fca6f9e541bb8
MohammedAlabd <mu96abd@gmail.com>
2025-06-04 17:02:33 +0300

    implement can_operator_vote
    It will check if the operator has already vote, as well as if he have
    any stakes to allow it to vote

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -1,6 +1,9 @@
-use solana_program::program_error::ProgramError;
+use solana_program::{program_error::ProgramError, pubkey::Pubkey};
 
-use crate::{constants::MAX_REALLOC_BYTES, error::NCNProgramError};
+use crate::{
+    ballot_box::BallotBox, constants::MAX_REALLOC_BYTES, epoch_snapshot::OperatorSnapshot,
+    error::NCNProgramError,
+};
 
 /// Calculate new size for reallocation, capped at target size
 /// Returns the minimum of (current_size + MAX_REALLOC_BYTES) and target_size
@@ -19,4 +22,24 @@ pub fn assert_ncn_program_error<T>(
 ) {
     assert!(test_error.is_err());
     assert_eq!(test_error.err().unwrap(), ncn_program_error);
+}
+
+pub fn can_operator_vote(
+    ballot_box: BallotBox,
+    operator_snapshot: OperatorSnapshot,
+    operator: &Pubkey,
+) -> bool {
+    let did_operator_vote = ballot_box.did_operator_vote(operator);
+
+    if did_operator_vote {
+        return false;
+    }
+
+    let stake_weight = operator_snapshot.stake_weights().stake_weight();
+
+    if stake_weight == 0 {
+        return false;
+    }
+
+    false
 }
